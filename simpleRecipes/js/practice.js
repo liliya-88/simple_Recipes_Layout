ad4d319a4be66afa3174c4edb2d5001a667d8ea6
master <666Lilly888@gmail.com>
2023-02-10 10:46:58 +0300

    hjncas

--- simpleRecipes/js/practice.js
+++ simpleRecipes/js/practice.js
@@ -1,78 +1,78 @@
-"use stcrict";
+// "use stcrict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
-  form.addEventListener("submit", formSend);
-  async function formSend(e) {
-    e.preventDefault();
+// window.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("form");
+//   form.addEventListener("submit", formSend);
+//   async function formSend(e) {
+//     e.preventDefault();
 
-    let error = formValidate(form);
-    let formData = new FormData(form);
-  
-    if (error === 0) {
-      let response = await fetch("sendmail.php", {
-        method: "POST",
-        body: formData
-      });
-      if (response.ok) {
-        form.classList.add("_sending");
-       
-        let result = await response.json();
-        alert(result.message);
-        formPreview.innerHTML = "";
-        form.reset();
-        form.classList.remove("_sending");
-      } else {
-        alert("Error!");
-        form.classList.remove("_sending");
-      }
+//     let error = formValidate(form);
+//     let formData = new FormData(form);
 
-    } else {
-      alert("Fill out the field, please!");
-    }
-    if (error === 0) {
-    } else {
-      alert("Fill out this field, please");
-    }
-  }
+//     if (error === 0) {
+//       let response = await fetch("sendmail.php", {
+//         method: "POST",
+//         body: formData,
+//       });
+//       if (response.ok) {
+//         form.classList.add("_sending");
 
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll("._reqired");
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
+//         let result = await response.json();
+//         alert(result.message);
+//         formPreview.innerHTML = "";
+//         form.reset();
+//         form.classList.remove("_sending");
+//       } else {
+//         alert("Error!");
+//         form.classList.remove("_sending");
+//       }
 
-      if (input.classList.contains("_email")) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else if (
-        input.getAttribute("type") === "checkbox" &&
-        input.checked === false
-      ) {
-        formAddError(input);
-        error++;
-      } else {
-        if (input.value === "") {
-          error++;
-        }
-      }
-    }
-    return error;
-  }
+//     } else {
+//       alert("Fill out the field, please!");
+//     }
+//     if (error === 0) {
+//     } else {
+//       alert("Fill out this field, please");
+//     }
+//   }
 
-  function formAddError(input) {
-    input.parentElement.classList.add("_error");
-    input.classList.add("_error");
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove("_error");
-    input.classList.remove("_error");
-  }
-  /* Check email */
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
-});
+//   function formValidate(form) {
+//     let error = 0;
+//     let formReq = document.querySelectorAll("._reqired");
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index];
+//       formRemoveError(input);
+
+//       if (input.classList.contains("_email")) {
+//         if (emailTest(input)) {
+//           formAddError(input);
+//           error++;
+//         }
+//       } else if (
+//         input.getAttribute("type") === "checkbox" &&
+//         input.checked === false
+//       ) {
+//         formAddError(input);
+//         error++;
+//       } else {
+//         if (input.value === "") {
+//           error++;
+//         }
+//       }
+//     }
+//     return error;
+//   }
+
+//   function formAddError(input) {
+//     input.parentElement.classList.add("_error");
+//     input.classList.add("_error");
+//   }
+//   function formRemoveError(input) {
+//     input.parentElement.classList.remove("_error");
+//     input.classList.remove("_error");
+//   }
+//   /* Check email */
+//   function emailTest(input) {
+//     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+//   }
+// });
